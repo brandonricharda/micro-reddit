cfd994da26598a93778340134d6ce44f6e873aba
brandonricharda <brandonricharda@gmail.com>
2020-12-28 23:27:49 -0500

    Initial commit.

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { in: 4..12 }, uniqueness: true
+    validates :password, presence: true, length: { in: 4..12 }
+    validates :email, presence: true
+end
