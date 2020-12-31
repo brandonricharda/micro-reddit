c06674e659cc2607bdde3573139ee7332fcffa2c
brandonricharda <brandonricharda@gmail.com>
2020-12-31 01:16:59 -0500

    Adds User and Post associations.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { in: 4..12 }
     validates :text, presence: true
     validates :user_id, presence: true
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { in: 4..12 }, uniqueness: true
     validates :password, presence: true, length: { in: 4..12 }
     validates :email, presence: true
+    has_many :posts
 end
