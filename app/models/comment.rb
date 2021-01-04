82792318f7fd4c23eab3d620539e4a84db71bb1e
brandonricharda <brandonricharda@gmail.com>
2021-01-03 19:24:32 -0500

    Adds comment model associations.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
     validates :text, presence: true, length: { in: 1..180 }
     validates :user_id, presence: true
     validates :post_id, presence: true
+    belongs_to :post
+    belongs_to :user
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length: { in: 4..12 }
+    validates :title, presence: true, length: { in: 4..30 }
     validates :text, presence: true
     validates :user_id, presence: true
     belongs_to :user
+    has_many :comments
 end
